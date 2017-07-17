1f0b039d1088c7e110a920fb4f58cc1a9c09a18f
beomi <latheledusjp@gmail.com>
2017-07-17 17:22:57 +0900

    update code to class

--- 0717_this_in_class/app.js
+++ 0717_this_in_class/app.js
@@ -1,32 +1,32 @@
 function onLoadFunction() {
     const ud = new updateDocument()
-
     const navigation = document.getElementById('navigation')
-    navigation.addEventListener('click', ud.onClickFunction.bind(ud))
     const section = document.querySelector('section')
-    ud.setValues(section)
-
-
     const blogBtn = document.querySelector('.blog-ajax-btn')
-    blogBtn.addEventListener('click', ud.loadBlogData.bind(ud))
-
     const blogIterBtn = document.querySelector('.blog-ajax-iter-btn')
+
+    navigation.addEventListener('click', ud.onClickFunction.bind(ud))
+    ud.setValues(section)
+    blogBtn.addEventListener('click', ud.loadBlogData.bind(ud))
     blogIterBtn.addEventListener('click', ud.loadIterBlogData.bind(ud))
 }
 
 class updateDocument {
     onClickFunction (e) {
-        if (e.target && e.target.matches('div.tab')) {
-            const selectedTab = document.getElementsByClassName('selectedTab')[0]
-            selectedTab.classList.remove('selectedTab')
-            e.target.classList.add('selectedTab')
-            const selectedSection = document.getElementsByClassName('eleDisplayShow')[0]
-            selectedSection.classList.remove('eleDisplayShow')
-            const tabId = e.target.getAttribute('id')
-            const newSelection = document.getElementById('my_' + tabId)
-            newSelection.classList.add('eleDisplayShow')
-            this.setValues(newSelection)
+        if (!(e.target && e.target.matches('div.tab'))) {
+            return false
         }
+        const selectedTab = document.getElementsByClassName('selectedTab')[0]
+        const selectedSection = document.getElementsByClassName('eleDisplayShow')[0]
+        const tabId = e.target.getAttribute('id')
+        const newSelection = document.getElementById('my_' + tabId)
+
+        selectedTab.classList.remove('selectedTab')
+        e.target.classList.add('selectedTab')
+        selectedSection.classList.remove('eleDisplayShow')
+        newSelection.classList.add('eleDisplayShow')
+
+        this.setValues(newSelection)
     }
 
     ajax(url, func) {
@@ -38,15 +38,16 @@ class updateDocument {
 
     setValues(section) {
         const sectionId = section.dataset.pk
-        if (!section.innerText) {
-            this.ajax('//jsonplaceholder.typicode.com/posts/' + sectionId, function (xhr) {
-                const json = JSON.parse(this.responseText)
-                const template = _.template(
-                    "<h2><%= title %></h2><p><%= body %></p>"
-                )
-                section.innerHTML = template(json)
-            })
+        if (section.innerText) {
+            return false
         }
+        this.ajax('//jsonplaceholder.typicode.com/posts/' + sectionId, function () {
+            const json = JSON.parse(this.responseText)
+            const template = _.template(
+                "<h2><%= title %></h2><p><%= body %></p>"
+            )
+            section.innerHTML = template(json)
+        })
     }
 
     loadBlogData() {
