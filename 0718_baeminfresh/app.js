b22aad9f1ab2b7d75a4a3143339900b2c57c69f2
beomi <latheledusjp@gmail.com>
2017-07-18 16:41:30 +0900

    works: 왼쪽오른쪽 메뉴버튼

--- 0718_baeminfresh/app.js
+++ 0718_baeminfresh/app.js
@@ -1,4 +1,6 @@
 const slidesPagination = document.querySelector('.slides_pagination')
+const slidesPrev = document.querySelector('.slides_prev')
+const slidesNext = document.querySelector('.slides_next')
 
 slidesPagination.addEventListener('click', function (e) {
     const targetId = Number(e.target.textContent) + 1
@@ -11,12 +13,48 @@ slidesPagination.addEventListener('click', function (e) {
     fadeIn(newLI)
     newLI.style.display = ''
     newLI.style.zIndex = 50
+    oldLI.style.zIndex = 0
+    oldLI.style.display = 'none'
+})
+
+slidesPrev.addEventListener('click', function () {
+    const oldLI = document.querySelector('.main_slides_lst > li:not([style*="display: none"])')
+    let newLI
+    if (oldLI.className.replace('s', '') <= 1) {
+        const childElementCount = oldLI.parentElement.children
+        newLI = childElementCount[childElementCount.length - 1]
+    } else {
+        newLI = oldLI.previousElementSibling
+    }
+
+    fadeOut(oldLI)
+    fadeIn(newLI)
+    newLI.style.display = ''
+    newLI.style.zIndex = 50
+    oldLI.style.zIndex = 0
+    oldLI.style.display = 'none'
+})
+
+slidesNext.addEventListener('click', function () {
+    const oldLI = document.querySelector('.main_slides_lst > li:not([style*="display: none"])')
+    const childElementCount = oldLI.parentElement.children
+    let newLI
+    if (oldLI.className.replace('s', '') >= oldLI.parentElement.children.length) {
+        newLI = childElementCount[0]
+    } else {
+        newLI = oldLI.nextElementSibling
+    }
+
+    fadeOut(oldLI)
+    fadeIn(newLI)
+    newLI.style.display = ''
+    newLI.style.zIndex = 50
 
     oldLI.style.zIndex = 0
     oldLI.style.display = 'none'
 })
 
-function fadeOut(el){
+function fadeOut(el) {
     el.style.opacity = 1;
     (function fade() {
         if ((el.style.opacity -= .1) < 0) {
@@ -27,7 +65,7 @@ function fadeOut(el){
     })();
 }
 
-function fadeIn(el){
+function fadeIn(el) {
     el.style.opacity = 0;
     (function fade() {
         let val = parseFloat(el.style.opacity)
