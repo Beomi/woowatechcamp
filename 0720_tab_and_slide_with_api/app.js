5866981bccc1d900f155e409f510afd194723006
Beomi <latheledusjp@gmail.com>
2017-07-23 23:02:44 +0900

    update for now......TODO: Update Tab-JS Class & Carousel before func

--- 0720_tab_and_slide_with_api/app.js
+++ 0720_tab_and_slide_with_api/app.js
@@ -1,118 +1,177 @@
-const slidesPagination = document.querySelector('.slides_pagination')
-const slidesPrev = document.querySelector('.slides_prev')
-const slidesNext = document.querySelector('.slides_next')
-
-
-slidesPagination.addEventListener('click', function (e) {
-    const targetId = Number(e.target.textContent) + 1
-    const oldLI = document.querySelector(".selected")
-    const newLI = document.querySelector(`.s${targetId}`)
-
-    if (oldLI === newLI) return false
-    fadeInOutWrapper(newLI, oldLI)
-})
-
-slidesPrev.addEventListener('click', function () {
-    const oldLI = document.querySelector(".selected")
-    const childElementCount = oldLI.parentElement.children
-    let newLI
-    if (oldLI.className.replace('s', '').replace('selected', '') <= 1) {
-        newLI = childElementCount[childElementCount.length - 1]
-    } else {
-        newLI = oldLI.previousElementSibling
+class Slide {
+    constructor() {
+        this.slidesPagination = document.querySelector('.slides_pagination')
+        this.slidesPrev = document.querySelector('.slides_prev')
+        this.slidesNext = document.querySelector('.slides_next')
     }
-    fadeInOutWrapper(newLI, oldLI)
-})
 
-slidesNext.addEventListener('click', arrowNext);
-
-function arrowNext() {
-    const oldLI = document.querySelector(".selected")
-    const siblingElements = oldLI.parentElement.children
-    const childElementCountIndex = siblingElements.length
-    let newLI
-    if (oldLI.className.replace('s', '').replace('selected', '') >= childElementCountIndex) {
-        newLI = siblingElements[0]
-    } else {
-        newLI = oldLI.nextElementSibling
-    }
-    fadeInOutWrapper(newLI, oldLI)
-}
-
-function fadeInOut(newLI, oldLI) {
-    (function fade() {
-        if (parseFloat(newLI.style.opacity) <= 1) {
-            newLI.style.opacity = parseFloat(newLI.style.opacity) + 0.05
+    arrowNext() {
+        const oldLI = document.querySelector(".selected")
+        const siblingElements = oldLI.parentElement.children
+        const childElementCountIndex = siblingElements.length
+        let newLI
+        if (oldLI.className.replace('s', '').replace('selected', '') >= childElementCountIndex) {
+            newLI = siblingElements[0]
+        } else {
+            newLI = oldLI.nextElementSibling
         }
-        if (parseFloat(oldLI.style.opacity) >= 0) {
-            oldLI.style.opacity = parseFloat(oldLI.style.opacity) - 0.05
-        }
-        requestAnimationFrame(fade)
-    })()
-}
-
-function fadeInOutWrapper(newLI, oldLI) {
-    newLI.style.display = ''
-    oldLI.style.display = ''
-    oldLI.style.zIndex = 0
-    newLI.style.opacity = 0
-    fadeInOut(newLI, oldLI)
-    oldLI.style.opacity = 0
-    newLI.style.zIndex = 50
-    oldLI.classList.remove('selected')
-    newLI.classList.add('selected')
-}
-
-function autoPlay(a) {
-    if (slidesNext.click()) {
-        return;
+        this.fadeInOutWrapper(newLI, oldLI)
     }
-    setInterval(arrowNext(), 2900);
-    setTimeout(() => {
-        autoPlay();
-    }, 3000);
-}
 
-// autoPlay();
-
-
-// Baemin Menus
-function transePosition(wrapper, direction, moveCount) {
-    if (direction === 'next') {
-        const animationPixel = 215 * moveCount
-        const innerWrapper = wrapper.children[0]
-        const wrapperClone = wrapper.cloneNode(true)
-        const innerWrapperClone = wrapperClone.children[0]
-
-        // animation
-        innerWrapper.style.transform = `translateX(-${animationPixel}px)`
-        // dom update
-        innerWrapper.addEventListener('transitionend', function () {
-            for (let i = 0; i < moveCount; i++) {
-                const firstCloneNode = innerWrapperClone.children[0]
-                innerWrapperClone.appendChild(firstCloneNode)
+    fadeInOut(newLI, oldLI) {
+        (function fade() {
+            if (parseFloat(newLI.style.opacity) <= 1) {
+                newLI.style.opacity = parseFloat(newLI.style.opacity) + 0.05
             }
-            wrapper.innerHTML = wrapperClone.innerHTML
+            if (parseFloat(oldLI.style.opacity) >= 0) {
+                oldLI.style.opacity = parseFloat(oldLI.style.opacity) - 0.05
+            }
+            requestAnimationFrame(fade)
+        })()
+    }
+
+    fadeInOutWrapper(newLI, oldLI) {
+        newLI.style.display = ''
+        oldLI.style.display = ''
+        oldLI.style.zIndex = 0
+        newLI.style.opacity = 0
+        this.fadeInOut(newLI, oldLI)
+        oldLI.style.opacity = 0
+        newLI.style.zIndex = 50
+        oldLI.classList.remove('selected')
+        newLI.classList.add('selected')
+    }
+
+    autoPlay(a) {
+        if (this.slidesNext.click()) {
+            return;
+        }
+        setInterval(this.arrowNext(), 2900);
+        setTimeout(() => {
+            this.autoPlay();
+        }, 3000);
+    }
+
+    registerEvent() {
+        this.slidesPagination.addEventListener('click', function (e) {
+            const targetId = Number(e.target.textContent) + 1
+            const oldLI = document.querySelector(".selected")
+            const newLI = document.querySelector(`.s${targetId}`)
+
+            if (oldLI === newLI) return false
+            this.fadeInOutWrapper(newLI, oldLI)
         })
 
-    } else if (direction === 'before') {
+        this.slidesPrev.addEventListener('click', () => {
+            const oldLI = document.querySelector(".selected")
+            const childElementCount = oldLI.parentElement.children
+            let newLI
+            if (oldLI.className.replace('s', '').replace('selected', '') <= 1) {
+                newLI = childElementCount[childElementCount.length - 1]
+            } else {
+                newLI = oldLI.previousElementSibling
+            }
+            this.fadeInOutWrapper(newLI, oldLI)
+        })
+
+        this.slidesNext.addEventListener('click', this.arrowNext.bind(this))
+    }
+
+}
+
+
+class Carousel {
+    constructor(carouselCount) {
+        this.carouselCount = carouselCount
+        this.leftBtn = document.querySelector('.carousel-left-btn')
+        this.rightBtn = document.querySelector('.carousel-right-btn')
+    }
+
+    transePosition(wrapper, direction, moveCount) {
+        if (direction === 'next') {
+            const animationPixel = 215 * moveCount
+            const innerWrapper = wrapper.children[0]
+            const wrapperClone = wrapper.cloneNode(true)
+            const innerWrapperClone = wrapperClone.children[0]
+
+            // animation
+            innerWrapper.style.transform = `translateX(-${animationPixel}px)`
+            // dom update
+            innerWrapper.addEventListener('transitionend', function () {
+                for (let i = 0; i < moveCount; i++) {
+                    const firstCloneNode = innerWrapperClone.children[0]
+                    innerWrapperClone.appendChild(firstCloneNode)
+                }
+                wrapper.innerHTML = wrapperClone.innerHTML
+            })
+
+        } else if (direction === 'before') {
+            // TODO: Update before logic
+        }
+    }
+
+    nextMenu() {
+        const carouselWrapper = document.querySelector('.carousel-wrapper')
+        this.transePosition(carouselWrapper, 'next', this.carouselCount)
+    }
+
+    beforeMenu() {
+        const carouselWrapper = document.querySelector('.carousel-wrapper')
+        this.transePosition(carouselWrapper, 'before', this.carouselCount)
+    }
+
+    registerEvent() {
+        this.leftBtn.addEventListener(
+            'click', this.beforeMenu.bind(this)
+        )
+        this.rightBtn.addEventListener(
+            'click', this.nextMenu.bind(this)
+        )
+    }
+}
+
+
+class Tab {
+    constructor() {
+        this.template = function () {
+            fetch('/templates/tabContent.html').then(function (response) {
+                return response.querySelector('#tabContent').innerHTML
+            })
+        }
+        this.apiBaseUrl = 'http://52.78.212.27:8080'
+    }
+
+    getData(type, hash) {
+        if (hash) {
+            fetch(this.apiBaseUrl + `/woowa/${type}/${hash}`)
+        } else {
+            fetch(this.apiBaseUrl + `/woowa/${type}`)
+        }
+    }
+
+    renderTemplate(data) {
+
+    }
+
+    updateDocument() {
+
+    }
+
+    registerEvent() {
 
     }
 }
 
-function nextMenu() {
-    const carouselWrapper = document.querySelector('.carousel-wrapper')
-    transePosition(carouselWrapper, 'next', 3)
-}
 
-function beforeMenu() {
-    const carouselWrapper = document.querySelector('.carousel-wrapper')
-    transePosition(carouselWrapper, 'before', 3)
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const slide = new Slide()
+    const carousel = new Carousel(3)
+    const tab = new Tab()
 
-document.querySelector('.carousel-left-btn').addEventListener(
-    'click', beforeMenu
-)
-document.querySelector('.carousel-right-btn').addEventListener(
-    'click', nextMenu
-)
+    slide.registerEvent()
+    slide.autoPlay()
+
+    carousel.registerEvent()
+
+    tab.registerEvent()
+})
