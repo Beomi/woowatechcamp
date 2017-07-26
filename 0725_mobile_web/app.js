7699a351bc243be4451a4adb2fdee0c12287580b
beomi <latheledusjp@gmail.com>
2017-07-26 09:21:18 +0900

    Draft: slider by touch

--- 0725_mobile_web/app.js
+++ 0725_mobile_web/app.js
@@ -1,5 +1,6 @@
 class Slider {
-    constructor (sliderWrapper) {
+    constructor (viewPort, sliderWrapper) {
+        this.viewPort = viewPort
         this.sliderWrapper = sliderWrapper
         this.firstXPosition = 0
         this.currentWrapperPosition = 0
@@ -8,44 +9,40 @@ class Slider {
 
     setFirstXPosition (xPosition) {
         this.firstXPosition = xPosition
-        this.startTime = new Date().getTime()
     }
 
     getDistance(xPosition) {
         return xPosition - this.firstXPosition
     }
 
-    getUptime() {
-        return (new Date().getTime() - this.startTime) / 1000
-    }
-
-    getVelocity(xPosition) {
-        const uptime = this.getUptime()
-        const movingPosition = this.getDistance(xPosition)
-        return movingPosition / uptime
-    }
-
     moveSliderWrapper(movingPixel) {
+        if (movingPixel > 0) {
+            return false
+        }
         this.sliderWrapper.style.transform = `translateX(${movingPixel}px)`
     }
 
     setEventListener() {
-        this.sliderWrapper.addEventListener('touchstart', e => {
+        this.viewPort.addEventListener('touchstart', e => {
+            e.preventDefault()
             this.sliderWrapper.style.transition = null
-            const newPosition = this.currentWrapperPosition + this.getDistance(e.changedTouches[0].pageX)
+            this.currentWrapperPosition = 0 || Number(this.sliderWrapper.style.transform.replace('translateX(', '').replace('px)', ''))
+            const newPosition = e.changedTouches[0].screenX
             this.setFirstXPosition(newPosition)
         })
-        this.sliderWrapper.addEventListener('touchmove', e => {
-            const newPixel = e.changedTouches[0].pageX
-            this.moveSliderWrapper(this.getDistance(newPixel))
+        this.viewPort.addEventListener('touchmove', e => {
+            e.preventDefault()
+            const newPixel = e.changedTouches[0].screenX
+            console.log(this.currentWrapperPosition + this.getDistance(newPixel))
+            this.moveSliderWrapper(
+                this.currentWrapperPosition + this.getDistance(newPixel)
+            )
         })
-        this.sliderWrapper.addEventListener('touchend', e => {
-            // TODO: Velocity ease time
+        this.viewPort.addEventListener('touchend', e => {
             const windowWidth = parseInt(window.innerWidth)
-            const distance = this.getDistance(e.changedTouches[0].pageX)
+            const distance = this.getDistance(e.changedTouches[0].screenX)
             this.sliderWrapper.style.transition = 'ease 1s'
 
-            console.log(distance)
             if ((Math.abs(distance)) > (windowWidth / 5)) {
                 if (distance < 0) {
                     this.currentWrapperPosition = this.currentWrapperPosition - windowWidth
@@ -62,4 +59,4 @@ class Slider {
     }
 }
 
-const slider = new Slider(document.querySelector('#sliderWrapper'))
+const slider = new Slider(document.querySelector('#viewPort'), document.querySelector('#sliderWrapper'))
